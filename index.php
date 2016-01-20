bfaa5bf884d42a0fc33dea44461958d466fd3fde
Steven Buxton <breathesteve@gmail.com>
2016-01-20 00:08:30 -0600

    POST Example

--- index.php
+++ index.php
@@ -1,23 +1,32 @@
 <!doctype html>
 <html>
-<head>
-<title>Webpage</title>
-<meta charset="utf-8" />
-<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-</head>
+	<head>
+		<title>Webpage</title>
+
+		<meta charset="utf-8" />
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+	</head>
 	<body>
 		<div>
-			<?php				 	
-				if ($_GET["submit"]) {
-				 if ($_GET["name"]) {
-				 echo "Your name is ".$_GET['name'];
-				 } else {		
-			 		echo "Please enter your name";		
-				 }			
-				}	 	
+			<?php
+				$names = array('Steven', 'Sean', 'John');
+
+				if ($_POST["submit"]) {
+				if ($_POST["name"]) {
+
+				foreach ($names as $name) {
+				if ($POST[‘name’] == $name) echo “I know you! Your name is “.
+				$name;
+				$knowYou = 1;
+				}
+				if (!$knowYou) echo “I don’t know you, “.$name;
+				 } else {
+				 echo "Please enter your name";
+				 }
+				}		 	
 			?>
-		<form>
+		<form method="post">
 			<label for="name">Name</label>
 			<input name="name" type="text" />
 			<input type="submit" name="submit" value="Submit Your Name" />
